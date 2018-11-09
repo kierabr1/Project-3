d28191b9a9c085389918603bf5c81846cc6084c7
kierabr1 <kiera.brown@bison.howard.edu>
2018-11-09 18:03:19 -0500

    Update- I added an integer value to compare it to the length of the user input to make sure the string is not too long

--- project2.asm
+++ project2.asm
@@ -5,9 +5,12 @@ str:
 
 main:
 li $v0, 8     #takes user input
-la $a0, str
-li $a1, 64
-
+la $a0, str  #stores string in register
+li $a1, 64   
+li $v0, 1
+la $s0, 4   #stores integer in register
+bne $a0, $s0, exit  #compares integer to string size 
+ 
 li $v0, 10  //exit call
 syscall
 
