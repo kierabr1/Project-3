38f353aab7a672ed1c5a17208e41ebf5e8810afb
kierabr1 <kiera.brown@bison.howard.edu>
2018-11-12 18:30:03 -0500

    Update- I created the label for the loop that I plan to use when checking to see if the input is valid or not

--- project2.asm
+++ project2.asm
@@ -7,6 +7,8 @@ main:
 li $v0, 8     #takes user input
 la $a0, str  #stores string in register
 li $a1, 64   
+loop:
+
 li $v0, 1
 la $s0, 4   #stores integer in register
 bne $a0, $s0, exit  #compares integer to string size 
