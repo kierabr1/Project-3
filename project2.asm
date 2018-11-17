c018cc618ada1f13cebaab95b352acec7a3cfef0
kierabr1 <kiera.brown@bison.howard.edu>
2018-11-16 20:22:40 -0500

    Update- After all the spaces are removed, I jump to another branch where I will see if the string is valid. In the middle of RemoveFirstSpaces, when a space is found it branches off to RemoveFirstSpace to remove that singular space and jump back to the RemoveFirstSpaces

--- project2.asm
+++ project2.asm
@@ -15,6 +15,11 @@ RemoveFirstSpaces:  #Remove leading spaces
 	lb $t9, 0($a0)
 	beq $t8, $t9, removeFirstSpace #remove space if dtected
 	move $t9, $a0
+	j Qualifications
+
+removeFirstSpace:   #Removes one space
+	addi $a0, $a0, 1
+	j removeFirstSpace
 IsEmptyMessage:
 	la $a0, IsEmpty
 	li $v0, 4
