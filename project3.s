9144e65cc33b2de6d0db0759f0e1ee246bbaf312
kierabr1 <kiera.brown@bison.howard.edu>
2018-12-14 14:30:50 -0800

    Update- If nothing is found, reset the pointer, else if a space is not found then keep looping until one of the branches is equal. In CheckValidityTwo, I'm checking to make sure that there isnt anymore characters after a space because that would be invalid

--- project3.s
+++ project3.s
@@ -44,7 +44,14 @@ CheckValidityOne:                        # This label iterates through the strin
        lb $s2, 0($t2)
 	   addi $t2, $t2, 1			#increment counter and pointer
        addi $t4, $t4, 1
-       beq $s2, 10, resetButtonOne  #restart if newline is found    
+       beq $s2, 10, resetButtonOne  #restart if newline is found 
+	   beq $s2, 0, resetButtonOne      #restart if nothing is found
+       bne $s2, 32, CheckValidityOne    # If a space is not found, then loop
+
+
+CheckValidityTwo:                  # Check to see if there is another set of characters after the space found
+       lb $s2, 0($t2)                  
+       addi $t2, $t2, 1         # increment pointer   
 FindLength:   #Count the characters in the string
 	addi $t0, $t0, 0  #Initialize count to zero
 	addi $t1, $t1, 10  #adds character to t1
